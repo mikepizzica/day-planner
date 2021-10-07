35c57e9171241b8efe80fc471a933370cc58023a
Mike Pizzica <mike.pizzica@gmail.com>
2021-10-07 19:47:01 -0400

    add all time slots and color coding javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,35 @@
 // current date at top of page
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+console.log(moment().hours());
+var currentHour = moment().hours();
+
+var allTimeBlocks = document.querySelectorAll("textarea");
+var allSaveButtons = document.querySelectorAll("button");
+
+// console.log(allTimeBlocks);
+
+for( i=0; i<allTimeBlocks.length; i++){
+    console.log(allTimeBlocks[i].id);
+    if(parseInt(allTimeBlocks[i].id) < currentHour){
+        // allTimeBlocks[i].className+=" past";
+        allTimeBlocks[i].classList.add("past");
+    }
+    else if(parseInt(allTimeBlocks[i].id) === currentHour){
+        // allTimeBlocks[i].className+=" past";
+        allTimeBlocks[i].classList.add("present");
+    }
+    else{
+        // allTimeBlocks[i].className+=" past";
+        allTimeBlocks[i].classList.add("future");
+    }
+}
+
+for( i=0; i<allSaveButtons.length; i++){
+    console.log(allSaveButtons[i].id);
+}
+
+allSaveButtons[0].addEventListener("click", function(event){
+    console.log(allTimeBlocks[0].value)
+});
